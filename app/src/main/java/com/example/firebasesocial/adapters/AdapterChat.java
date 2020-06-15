6aca26fd22fa1050bec4ebe9bc27a4042aa6bd9a
PKS <pkumarsaha71@gmail.com>
2020-06-15 18:38:46 +0600

    12

---
+++ app/src/main/java/com/example/firebasesocial/adapters/AdapterChat.java
@@ -0,0 +1,123 @@
+package com.example.firebasesocial.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.firebasesocial.R;
+import com.example.firebasesocial.models.ModelChat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
+
+    private static final int MSG_TYPE_LEFT = 0;
+    private static final int MSG_TYPE_RIGHT = 1;
+    Context context;
+    List<ModelChat> chatList;
+    String imageUrl;
+    FirebaseUser fUser;
+
+    public AdapterChat(Context context, List<ModelChat> chatList, String imageUrl) {
+        this.context = context;
+        this.chatList = chatList;
+        this.imageUrl = imageUrl;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        if(viewType ==MSG_TYPE_RIGHT){
+            View view = LayoutInflater.from(context).inflate(R.layout.row_chat_right,parent,false);
+            return  new MyHolder(view);
+
+        } else{
+            View view = LayoutInflater.from(context).inflate(R.layout.row_chat_left,parent,false);
+            return  new MyHolder(view);
+        }
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+        //get the data
+        String message = chatList.get(position).getMessage();
+        String timeStamp = chatList.get(position).getTimestrap();
+        //convert the time
+        Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+        calendar.setTimeInMillis(Long.parseLong(timeStamp));
+        String dateTime = java.text.DateFormat.getDateTimeInstance().format(new Date());
+
+        holder.messageTv.setText(message);
+        holder.timeTV.setText(dateTime);
+        try{
+            Picasso.get().load(imageUrl).into(holder.profileImage);
+        } catch (Exception e){
+
+        }
+
+        //set sent or delivered sattus
+        if(position == chatList.size()-1){
+            if(chatList.get(position).isSeen()){
+                holder.issenTV.setText("seen");
+            } else{
+                holder.issenTV.setText("delivered");
+            }
+        } else{
+
+            holder.issenTV.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        //get currently signed in user
+        fUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(chatList.get(position).getSender().equals(fUser.getUid())){
+
+            return MSG_TYPE_RIGHT;
+        }
+        else {
+            return MSG_TYPE_LEFT;
+        }
+
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder{
+
+    //views
+    ImageView profileImage;
+    TextView messageTv,timeTV, issenTV;
+
+    public MyHolder(@NonNull View itemView) {
+        super(itemView);
+
+        profileImage = itemView.findViewById(R.id.profileIvv);
+        messageTv = itemView.findViewById(R.id.messageTvv);
+        timeTV = itemView.findViewById(R.id.timeTv);
+        issenTV = itemView.findViewById(R.id.isSeenTv);
+    }
+}
+
+}
+
--- app/src/main/java/com/example/firebasesocial/models/ModelChat.java
+++ app/src/main/java/com/example/firebasesocial/models/ModelChat.java
@@ -1,4 +1,61 @@
 package com.example.firebasesocial.models;
 
 public class ModelChat {
+
+    String message,receiver,sender,timestrap;
+    boolean isSeen;
+
+    ModelChat(){
+
+    }
+
+    public ModelChat(String message, String receiver, String sender, String timestrap, boolean isSeen) {
+        this.message = message;
+        this.receiver = receiver;
+        this.sender = sender;
+        this.timestrap = timestrap;
+        this.isSeen = isSeen;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getTimestrap() {
+        return timestrap;
+    }
+
+    public void setTimestrap(String timestrap) {
+        this.timestrap = timestrap;
+    }
+
+    public boolean isSeen() {
+        return isSeen;
+    }
+
+    public void setSeen(boolean seen) {
+        isSeen = seen;
+    }
+
+
 }
